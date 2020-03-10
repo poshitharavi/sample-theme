bdf63d23b9c6554882dce871a5c0622abda71ca5
Poshitha Ravindu <raviposhitha@gmail.com>
2020-03-10 16:56:44 +0530

    created the folder structure with all functionalities

---
+++ view/dashboard.php
@@ -0,0 +1,10 @@
+<?php
+include_once('header.php');
+?>
+<section class="content">
+
+
+</section>
+<?php
+include_once('footer.php');
+?>
---
+++ view/footer.php
@@ -0,0 +1,62 @@
+<?php
+?>
+<!-- Footer -->
+<footer>
+    <div class="container-fluid">
+        <div class="row clearfix">
+            <div class="col-sm-6">
+                Copyright &copy; 2017, <b>AdminBSB - Sensitive</b>
+            </div>
+            <div class="col-sm-6 align-right">
+                Hand-crafted & Made with love :)
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- #END# Footer -->
+</div>
+
+<!-- Jquery Core Js -->
+<script src="../template/plugins/jquery/dist/jquery.min.js"></script>
+
+<!-- JQuery UI Js -->
+<script src="../template/plugins/jquery-ui/jquery-ui.js"></script>
+
+<!-- Bootstrap Core Js -->
+<script src="../template/plugins/bootstrap/dist/js/bootstrap.min.js"></script>
+
+<!-- Pace Loader Js -->
+<script src="../template/plugins/pace/pace.js"></script>
+
+<!-- Screenfull Js -->
+<script src="../template/plugins/screenfull/src/screenfull.js"></script>
+
+<!-- Metis Menu Js -->
+<script src="../template/plugins/metisMenu/dist/metisMenu.js"></script>
+
+<!-- Jquery Slimscroll Js -->
+<script src="../template/plugins/jquery-slimscroll/jquery.slimscroll.js"></script>
+
+<!-- Switchery Js -->
+<script src="../template/plugins/switchery/dist/switchery.js"></script>
+
+<!-- iCheck Js -->
+<script src="../template/plugins/iCheck/icheck.js"></script>
+
+<!-- Jquery Sparkline Js -->
+<script src="../template/plugins/jquery-sparkline/dist/jquery.sparkline.js"></script>
+
+<!-- Peity Js -->
+<script src="../template/plugins/peity/jquery.peity.js"></script>
+
+<!-- Custom Js -->
+<script src="../template/js/admin.js"></script>
+
+<!-- Google Analytics Code -->
+<script src="../template/js/google-analytics.js"></script>
+
+<!-- Demo Purpose Only -->
+<script src="../template/js/demo.js"></script>
+</body>
+
+</html>
